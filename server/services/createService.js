160189e5f3d4852826d6c21a4c1e964ad449fd41
Kelme13 <kelvinmelgar777@gmail.com>
2024-04-25 07:39:01 -0600

    tc1.0

--- server/services/createService.js
+++ server/services/createService.js
@@ -1,116 +1,123 @@
-const {DB_Config} = require("../config.js")
+const { DB_Config } = require("../config.js");
 const crypto = require("crypto");
 const { async } = require("q");
 
-
 const knex = require("knex")({
-    client: 'mysql2',
-    connection: {
-        host: DB_Config.TALENT_HUB_SERVER,
-        port: DB_Config.TALENT_HUB_PORT,
-        user: DB_Config.TALENT_HUB_USER,
-        password: DB_Config.TALENT_HUB_PASSWORD,
-        database: DB_Config.TALENT_HUB_DB,
-    },
+  client: "mysql2",
+  connection: {
+    host: DB_Config.TALENT_HUB_SERVER,
+    port: DB_Config.TALENT_HUB_PORT,
+    user: DB_Config.TALENT_HUB_USER,
+    password: DB_Config.TALENT_HUB_PASSWORD,
+    database: DB_Config.TALENT_HUB_DB,
+  },
 });
 
-
 async function agregarEmpleado(empleado) {
-    try {
-        const result = await knex.raw('CALL CrearEmpleado(?,?,?,?,?,?,?,?,?,?,?);', [
-            empleado.primer_nombre,
-            empleado.segundo_nombre,
-            empleado.primer_apellido,
-            empleado.segundo_apellido,
-            empleado.telefono,
-            empleado.fecha_nacimiento,
-            empleado.numero_identidad,
-            empleado.direccion,
-            empleado.correo,
-            empleado.id_perfil_puesto,
-            empleado.id_departamento
-        ]);
-        return result;
-    } catch(error) {
-        console.log(error);
-        return false;
-    }
-    
-};
-
-async function crearUsuario(usuarioData) {
-    try {
-        const result = await knex('usuarios').insert(usuarioData);
-
-        return result;
-    } catch(error) {
-        console.log(error);
-        return false;
-    }
-};
-
-async function crearEvaluacion(evaluacionData) {
-    try {
-    } catch(error) {
-        console.log(error);
-    }
+  try {
+    const result = await knex.raw(
+      "CALL CrearEmpleado(?,?,?,?,?,?,?,?,?,?,?);",
+      [
+        empleado.primer_nombre,
+        empleado.segundo_nombre,
+        empleado.primer_apellido,
+        empleado.segundo_apellido,
+        empleado.telefono,
+        empleado.fecha_nacimiento,
+        empleado.numero_identidad,
+        empleado.direccion,
+        empleado.correo,
+        empleado.id_perfil_puesto,
+        empleado.id_departamento,
+      ]
+    );
+    return result;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-async function crearCompetencia(competenciaData){
-    try {
-        const result = await knex('competencias').insert(competenciaData);
-    } catch(error) {
-        console.log(error);
-    }
+async function crearUsuario(usuarioData) {
+  try {
+    const result = await knex("usuarios").insert(usuarioData);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function crearEvaluacion(evaluacionData) {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function crearCompetencia(competenciaData) {
+  try {
+    const result = await knex("competencias").insert(competenciaData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function crearPregunta(preguntaData) {
-    try {
-        const result = await knex('evaluaciones_comportamientos').insert(preguntaData);
-        return result;
-    } catch(error) {
-        return false;
-    }
+  try {
+    const result = await knex("evaluaciones_comportamientos").insert(
+      preguntaData
+    );
+    return result;
+  } catch (error) {
+    return false;
+  }
 }
 
 async function crearEncuesta(encuestaData) {
-    try {
-        const result = await knex.raw('DECLARE @NuevoID INT; EXEC dbo.InsertarEncuesta ?, ?, ?, @NuevoID OUTPUT; SELECT @NuevoID AS IDGenerado', [
-            encuestaData.tipo_encuesta,
-            encuestaData.nombre_encuesta,
-            encuestaData.lenguaje
-        ]);
-    
-        //const nuevoID = result[0][0].NuevoID;
-        return(result[0].IDGenerado);
-    } catch (error) {
-        console.error(error);
-    }
-};
+  try {
+    const result = await knex.raw(
+      "DECLARE @NuevoID INT; EXEC dbo.InsertarEncuesta ?, ?, ?, @NuevoID OUTPUT; SELECT @NuevoID AS IDGenerado",
+      [
+        encuestaData.tipo_encuesta,
+        encuestaData.nombre_encuesta,
+        encuestaData.lenguaje,
+      ]
+    );
+
+    //const nuevoID = result[0][0].NuevoID;
+    return result[0].IDGenerado;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function crearPreguntaEncuesta(preguntaData) {
-    try {
-        const result = await knex('preguntas').insert(preguntaData);    
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-};
+  try {
+    const result = await knex("preguntas").insert(preguntaData);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function insertarHabilidades(habilidadData) {
-    try {
-        const result = await knex('perfil_puesto_habilidades').insert(habilidadData);
-    } catch(error) {
-        console.log(error);
-    }
-};
+  try {
+    const result = await knex("perfil_puesto_habilidades").insert(
+      habilidadData
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
-    agregarEmpleado,
-    crearUsuario,
-    crearCompetencia,
-    crearPregunta,
-    crearEncuesta,
-    crearPreguntaEncuesta,
-    insertarHabilidades
+  agregarEmpleado,
+  crearUsuario,
+  crearCompetencia,
+  crearPregunta,
+  crearEncuesta,
+  crearPreguntaEncuesta,
+  insertarHabilidades,
 };
